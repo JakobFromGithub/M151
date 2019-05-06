7c340b41e63fe9da84dbaea0a52eb7d6bf37c9a6
Dan Livingston <46896117+danlivingston@users.noreply.github.com>
2019-05-06 14:15:30 +0200

    mssql perms

--- mssql/create_toptipf_admin.sql
+++ mssql/create_toptipf_admin.sql
@@ -2,8 +2,8 @@ USE [master]
 GO
 
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [toptipf_admin]    Script Date: 06.05.2019 13:54:15 ******/
-CREATE LOGIN [toptipf_admin] WITH PASSWORD=N'vAn6spQ8mjIrWBzMwY3ALhe7x8C8+ggodTYGXAQZuXk=', DEFAULT_DATABASE=[TopTipf], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+/****** Object:  Login [toptipf_admin]    Script Date: 06.05.2019 14:12:38 ******/
+CREATE LOGIN [toptipf_admin] WITH PASSWORD=N'kSHsyPG1W1u1K1PDFtbhuaPDv3Gp3gtmQar2K+PlWXw=', DEFAULT_DATABASE=[TopTipf], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 
 ALTER LOGIN [toptipf_admin] DISABLE
--- mssql/create_toptipf_application.sql
+++ mssql/create_toptipf_application.sql
@@ -2,8 +2,8 @@ USE [master]
 GO
 
 /* For security reasons the login is created disabled and with a random password. */
-/****** Object:  Login [toptipf_application]    Script Date: 06.05.2019 13:54:45 ******/
-CREATE LOGIN [toptipf_application] WITH PASSWORD=N'Z+RC+AkJEpFpt3TRo9/5/s0m49t83+I8yI9Go9BQX/8=', DEFAULT_DATABASE=[TopTipf], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
+/****** Object:  Login [toptipf_application]    Script Date: 06.05.2019 14:12:29 ******/
+CREATE LOGIN [toptipf_application] WITH PASSWORD=N'z5NVWrs7V+xbDLy25P6XNuC/uapLSusTtk4qkE2P1Bw=', DEFAULT_DATABASE=[TopTipf], DEFAULT_LANGUAGE=[us_english], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 
 ALTER LOGIN [toptipf_application] DISABLE
